efc5247a6847bcee01fe7537181671fe8a05d46d
local <local@gmail.com>
2020-05-18 11:30:21 +0200

    Added test cases in resources

--- src/test/java/stepDefinitions/StepDefinitionsByDate.java
+++ src/test/java/stepDefinitions/StepDefinitionsByDate.java
@@ -11,9 +11,6 @@ import pojo.ResponceLatestUsdGbp;
 import resources.APIResources;
 import resources.Utils;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import static io.restassured.RestAssured.given;
 
 public class StepDefinitionsByDate extends Utils {
